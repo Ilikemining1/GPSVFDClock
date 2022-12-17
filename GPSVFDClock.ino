30fd6b96f23c8799a0f5aab4242fad57f3b90ea5
IlikeTech <spbach1234@gmail.com>
2022-12-17 18:19:09 -0500

    Clean up existing codebase

--- GPSVFDClock.ino
+++ GPSVFDClock.ino
@@ -1,13 +1,14 @@
+#include <DFRobot_VEML7700.h>
 #include <SparkFun_u-blox_GNSS_Arduino_Library.h>
-#include <LiquidCrystal.h>
+#include <CU40025_VFD.h>
 #include <RTClib.h>
-#include <TimeLib.h>
+#include <Wire.h>
 
+// ESP32 GPIO Pin Definitions:
 #define ubxRX 16
 #define ubxTX 17
 
 #define ubxPPS 18
-#define rtcPPS 19
 
 #define vfdRS 23
 #define vfdEN 25
@@ -16,34 +17,30 @@
 #define vfdD6 32
 #define vfdD7 33
 
+// Peripheral Handles:
 RTC_DS3231 rtc;
+DFRobot_VEML7700 als;
 SFE_UBLOX_GNSS ubxGNSS;
-LiquidCrystal vfd(vfdRS, vfdEN, vfdD4, vfdD5, vfdD6, vfdD7);
+CU40025_VFD vfd(vfdRS, vfdEN, vfdD4, vfdD5, vfdD6, vfdD7);
 
-volatile time_t currentTime;
 
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-char monthsOfTheYear[12][12] = {"January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+static char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+static char monthsOfTheYear[12][12] = {"January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
-bool usingGpsPPS = false;
-char *timeSource = "RTC";
-
-int initializeVFD() {
+int8_t initializeVFD(void) {
   Serial.println("VFD: initializing");
   vfd.begin(40, 2);
   return 0;
 }
 
-int initializeGNSS() {
-  int initCount = 0;
-  Serial.println("GNSS: initializing U-Blox GNSS module");
+int8_t initializeGNSS(void) {
+  uint8_t initCount = 0;
+  Serial.println("GNSS: initializing U-blox GNSS module");
   while (initCount < 5) {
-    Serial.println("GNSS: trying 115200 baud");
     Serial2.begin(115200, SERIAL_8N1, ubxRX, ubxTX);
     if (ubxGNSS.begin(Serial2) == true) break;
     
     delay(100);
-    Serial.println("GNSS: trying 9600 baud");
     Serial2.begin(9600, SERIAL_8N1, ubxRX, ubxTX);
      if (ubxGNSS.begin(Serial2) == true) {
       Serial.println("GNSS: connected at 9600 baud, switching to 115200");
@@ -52,12 +49,13 @@ int initializeGNSS() {
      } else {
       Serial.println("GNSS: Failed to initialize, retrying");
       initCount++;
-      delay(1000);
+      delay(200);
      }
   }
   if (initCount < 5) {
-    Serial.println("GNSS: connected to UBX module at 115200 baud");
+    Serial.println("GNSS: UBX initialized at 115.2k");
     ubxGNSS.setUART1Output(COM_TYPE_UBX);
+    ubxGNSS.setI2COutput(COM_TYPE_UBX);
     ubxGNSS.saveConfiguration();
     return 0;
   } else {
@@ -66,110 +64,59 @@ int initializeGNSS() {
   }
 }
 
-int initializeRTC() {
-  int initCount = 0;
-  Serial.println("RTC: initializing DS3231");
+int8_t initializeRTC(void) {
+  int8_t initCount = 0;
+  Serial.println("RTC: initializing");
   while (initCount < 5) {
     if (!rtc.begin()) {
       Serial.println("RTC: initialization failed, retrying");
       initCount++;
-      delay(1000);
+      delay(200);
     } else {
       break;
     }
   }
   if (initCount < 5) {
-    Serial.println("RTC: initialized successfully!");
+    Serial.println("RTC: initialized successfully");
   } else {
     return -1;
   }
 
   if (rtc.lostPower()) {
-    Serial.println("RTC: power loss detected, time set to default");
+    Serial.println("RTC: power loss detected, time set to default.");
     vfd.print("RTC: power loss detected, please set the time!");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
 
-  rtc.writeSqwPinMode(DS3231_SquareWave1Hz);
-
   return 0;
 }
 
-void IRAM_ATTR gpsTimepulse() {
-  currentTime++;
-}
-
-void IRAM_ATTR rtcTimepulse() {
-  currentTime++;
-}
-
-int changeInterrupts(bool useGps) {
-  if (useGps == true) {
-    detachInterrupt(rtcPPS);
-    attachInterrupt(ubxPPS, gpsTimepulse, RISING);
-    Serial.println("SYS: using GPS timepulses");
-  } else {
-    detachInterrupt(ubxPPS);
-    attachInterrupt(rtcPPS, rtcTimepulse, FALLING);
-    Serial.println("SYS: using RTC timepulses");
-  }
+int8_t initializeALS(void) {
+  Serial.println("ALS: initializing");
+  als.begin();
 
   return 0;
-}
-
-const char *formattedTimeString() {
-  char *amPm;
-  if (isPM(currentTime)) {
-    amPm = "PM";
-  } else {
-    amPm = "AM";
-  }
-
-  int numBytes = snprintf(NULL, 0, "%s %s %2d, %4d %02d:%02d:%02d %s", daysOfTheWeek[weekday(currentTime)], monthsOfTheYear[month(currentTime) - 1], day(currentTime), year(currentTime), hourFormat12(currentTime), minute(currentTime), second(currentTime), amPm) + 1;
-  char *timeString = (char*)malloc(numBytes);
-  snprintf(timeString, numBytes, "%s %s %2d, %4d %02d:%02d:%02d %s", daysOfTheWeek[weekday(currentTime)], monthsOfTheYear[month(currentTime) - 1], day(currentTime), year(currentTime), hourFormat12(currentTime), minute(currentTime), second(currentTime), amPm);
-
-  return timeString;
 }
 
 void setup() {
   Serial.begin(115200);
 
   pinMode(ubxPPS, INPUT);
-  pinMode(rtcPPS, INPUT_PULLUP);
   
   Serial.println("SYS: VFD GPS Clock initializing...");
-  Serial.printf("VFD: init status %d\n", initializeVFD());
-  Serial.printf("GNSS: init status %d\n", initializeGNSS());
-  Serial.printf("RTC: init status %d\n", initializeRTC());
+  
+  if ((initializeVFD() + initializeGNSS() + initializeRTC() + initializeALS())) {
+    Serial.println("SYS: hardware initialization failed, hanging now");
+    vfd.print("Hardware Initialization Failure!");
+    while(true);
+  } else {
+    Serial.println("SYS: hardware initialization complete");
+    vfd.print("GPS Clock with VFD\nPowered by U-blox and ESP32");
+  }
+  
 
-  Serial.println("SYS: Setting current time from RTC");
-  currentTime = rtc.now().unixtime();
-  attachInterrupt(rtcPPS, rtcTimepulse, FALLING);
-  Serial.printf("SYS: time is now %s, RTC interrupts enabled\n", formattedTimeString());
 }
 
 void loop() {
-  int fixType = ubxGNSS.getFixType();
-  int siv = ubxGNSS.getSIV();
-  
-  vfd.setCursor(0, 0);
-  vfd.print(formattedTimeString());
-  vfd.setCursor(0, 1);
-  vfd.printf("Fix Type: %d SIV: %d Time Source: %s", fixType, siv, timeSource);
-
-
- if(!usingGpsPPS && (fixType > 0)) {
-    timeSource = "GPS";
-    currentTime = ubxGNSS.getUnixEpoch();
-    rtc.adjust(DateTime(currentTime));
-    changeInterrupts(true);
-    usingGpsPPS = true;
-  } else if(usingGpsPPS && (fixType < 1)) {
-    timeSource = "RTC";
-    changeInterrupts(false);
-    usingGpsPPS = false;
-  }
-
-  delay(10);
+ 
 }
